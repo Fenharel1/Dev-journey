668345a47347843b71abd56788d6ffbffff6336f
Reinhard Conde <reyqsp@gmail.com>
2024-02-21 11:47:46 -0500

    creating users app backend

--- Spring/backend-cartapp/src/main/java/com/reinhard/backend/app/backendcartapp/controllers/ProductController.java
+++ Spring/backend-cartapp/src/main/java/com/reinhard/backend/app/backendcartapp/controllers/ProductController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @RestController
-@CrossOrigin(origins="http://localhost:5173")
+@CrossOrigin(origins={"http://localhost:5173", "http://192.168.1.10:5173"})
 @RequestMapping("/api")
 public class ProductController {
 
